9d83cb22660d67372da9fc66083a7d60473f4600
Edward Li <edwardyaoli@gmail.com>
2019-02-17 17:35:54 -0500

    new elevator extender opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ElevatorExtenderTestOpMode.kt
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.tests
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotor
+
+/**
+ * Tests REV Core Hex Motor on Elevator Extender
+ */
+@TeleOp(name = "Elevator Test", group = "Tests") // TODO: Add a good name
+class ElevatorExtenderTestOpMode: LinearOpMode() {
+
+    var extenderMotor: DcMotor? = null
+
+    override fun runOpMode() {
+
+        // TODO: Place any code that should run when INIT is pressed here
+
+        telemetry.addData("Status", "Initialized")
+        telemetry.update() // NOTE: In a LinearOpMode, you will need to call update() whenever you modify telemetry data.
+
+        extenderMotor = hardwareMap.get(DcMotor::class.java, "elevator_extender")
+
+        waitForStart()
+
+        // TODO: Place any code that should run once the op mode starts here.
+        // Use opModeIsActive() to check whether the robot should stop.
+
+        while (opModeIsActive()) {
+            val power = gamepad1.left_stick_y.toDouble()*0.2
+            extenderMotor!!.power = power
+        }
+
+        // If you want to stop the op mode once you're done, call requestOpModeStop().
+    }
+}
